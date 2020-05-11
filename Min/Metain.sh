5df8edf6a325f63efe3513642a04ddc759753015
did4510 <59131865+did4510@users.noreply.github.com>
2020-05-11 19:50:04 +0530

    Create Metain.sh

---
+++ Min/Metain.sh
@@ -0,0 +1,57 @@
+ 
+#colour section
+red='\033[1;31m'
+rset='\033[0m'
+grn='\033[1;32m'
+ylo='\033[1;33m'
+#script coding starts
+clear
+echo "  "
+echo '                           
+                        _ _          
+                       //\/\etasploit
+' | lolcat
+echo " "
+echo -e "$red                MeTaSpLoIt Is InStALlInG...$rset"
+echo "  "
+echo -e "$grn        [This May Take Time Up-to 30 Minutes so wait]$rset"
+echo " "
+sleep 6.0
+clear
+echo -e "$red                         Checking..>"
+sleep 2.0
+clear
+echo -e "$red                         Checking...>"
+sleep 2.0
+clear
+echo -e "$red                         Checking....>"
+sleep 2.0
+clear
+echo -e "$red                         Checking.....>"
+sleep 2.0
+clear
+echo -e "$grn                        Checking......>"
+sleep 2.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+cd $HOME
+if [ -d $HOME/etasploit-framework ]; then
+echo -e "$ylo              Metasploit is Alerady installed...!$rset"
+else
+echo " "
+echo -e "$ylo                Select Device Version please.....$rset"
+echo " "
+echo " "
+sleep 2.0
+cd $H0ME
+cd m-fire
+cd min
+bash opti.sh 
+echo " "
+fi
+echo " "
